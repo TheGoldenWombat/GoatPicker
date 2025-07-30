f1970061b4abd5b9ea0fbfd3301915875948c8bb
TheGoldenWombat <thegoldenwombat@gmail.com>
2025-07-30 00:35:22 -0700

    Groundwork for missile sfx

--- projectile.gd
+++ projectile.gd
@@ -1,14 +1,20 @@
 class_name Projectile
 extends Area2D
 
+# TODO CLEAN UP THIS WHOLE THING
+# LOTS OF UNNECESSARY COMPLEXITY FROM EXPERIMENTING
+
 @export var projectile_tip: TextureRect
 @export var projectile_body: ColorRect
+@export var launch_sfx: AudioStreamPlayer2D
+@export var cruising_sfx: AudioStreamPlayer2D
+@export var explosion_sfx: AudioStreamPlayer2D
 
 var target_racer: Racer
 var target: Area2D
 
-var max_speed: float = 500.00
-var boost_multiplier: float = 5.0
+var max_speed: float = 1000.00
+var boost_multiplier: float = 3.0
 var current_speed: float
 
 const LAUNCH_DRAG_FACTOR: float = 0.01
@@ -41,6 +47,7 @@ func remove_projectile() -> void:
 func signal_on_hit() -> void:
 	emit_signal("on_hit", projectile_type)
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	current_speed = max_speed * boost_multiplier # Initial boost
@@ -49,7 +56,8 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-
+	
+	
 	# Move projectile toward target
 	if current_speed > max_speed:
 		var deceleration: float = max_speed * delta * boost_multiplier / 2.5
@@ -58,7 +66,6 @@ func _physics_process(delta: float) -> void:
 	if current_speed <= max_speed * (boost_multiplier * 0.66):
 		if current_drag_factor <= CRUISING_DRAG_FACTOR:
 			current_drag_factor = lerp(current_drag_factor, CRUISING_DRAG_FACTOR, 0.005)
-			print(current_drag_factor)
 		var direction: Vector2 = global_position.direction_to(target.global_position)
 		var desired_velocity: Vector2 = direction * current_speed
 		var change: Vector2 = (desired_velocity - current_velocity) * current_drag_factor
@@ -80,6 +87,7 @@ func _on_area_entered(area: Area2D) -> void:
 	print ("target: " + str(target))
 	print ("entered: " + str(area))
 	if area == target:
+		explosion_sfx.play()
 		target_racer.apply_projectile_effect(projectile_type)
 		target_racer.target_crosshair.hide()
 		target_racer.update_line()
